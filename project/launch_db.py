2943a42400a2f76f27b7777a7b891d312de682f5
Wanshuiquan <liheyang45@gmail.com>
2024-07-08 17:03:48 +0200

    start the project!

---
+++ project/launch_db.py
@@ -0,0 +1,3 @@
+import subprocess 
+subprocess.Popen("${MDB_HOME}/build/Release/bin/server_sparql ${MDB_HOME}/data/example-rdf-database", shell=True)
+# subprocess.Popen("python3 ${MDB_HOME}/scripts/sparql_query.py ${MDB_HOME}/data/example-sparql-query.rq", shell=True)
