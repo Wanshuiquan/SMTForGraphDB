08263d32b401a85b035643a6a1a051d06168f6bc
Wanshuiquan <liheyang45@gmail.com>
2024-09-24 22:31:20 +0200

    modified:   notes/algo/algo.pdf 	new file:   notes/syntax/syntax.pdf 	modified:   notes/syntax/syntax.tex 	modified:   project/launch_db.py

--- project/launch_db.py
+++ project/launch_db.py
@@ -1,13 +1,1 @@
-import subprocess 
-
-def launch_mql(path="$MDB_HOME/data/example-rdf-database"):
-    subprocess.Popen(f"$MDB_HOME/build/Release/bin/server_mql {path}", shell=True)
-# subprocess.Popen("python3 ${MDB_HOME}/scripts/sparql_query.py ${MDB_HOME}/data/example-sparql-query.rq", shell=True)
-
-
-def build_db(data, db):
-    subprocess.Popen(f"$MDB_HOME/build/Release/bin/create_db_mql {data} {db}",shell=True)
-
-
-if __name__ == "__main__":
-    launch_mql()
+# prepare data 
