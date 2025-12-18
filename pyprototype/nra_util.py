2dfd381c940693a118c0eb98d4a804b6d84665f1
heyang li <liheyang45@gmail.com>
2025-12-18 14:40:15 +0100

    new pyprototype for property graph

--- pyprototype/nra_util.py
+++ pyprototype/nra_util.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass 
-from typing import Set
+from typing import Set, Dict, Any, Tuple 
 import z3
+from itertools import product
+import networkx as nx 
 EPSILON = z3.Const("epsilon", z3.RealSort())
 
 @dataclass 
@@ -36,3 +38,46 @@ class NRA_Macro_State:
             return False
         
 
+@dataclass 
+class ParametricAutomaton:
+    initial_state: int
+    accepting_states: Set[int]
+    transitions: Dict[int, Tuple[Tuple[str, z3.Ast], int]]  # source_state -> ((edge_label, constraint), target_state)
+
+
+@dataclass
+class PropertyGraph:
+    edges: Dict[int, Tuple[Tuple[str, int]]]  # source_node ->  label, target_node)
+    nodes: Set[int] # (node_id, node_type)
+    attribute: Dict[Tuple[int, str], Any]  #node_id, attribute_name -> attribute_value
+
+def generate_property_graph(edge_num:int, node_num:int):
+      """
+      Docstring for generate_property_graph
+      
+      :param edge_num: Description
+      :type edge_num: int
+      :param node_num: Description
+      :type node_num: int
+      """
+
+@dataclass
+class ProductGraph:
+    nodes: Set[tuple[int, int]]  # (property_node_id, automaton_state)
+    edges: Dict[tuple[tuple[int, int], tuple[str, z3.Ast, tuple[int, int]]]]  # ((src_property_node_id, automaton_state), edge_label, (tgt_property_node_id, automaton_state))
+    
+    def __init__(self, prop_graph: PropertyGraph, automaton: ParametricAutomaton):
+        self.nodes = set()
+        self.edges = dict()
+        self.construct_product(prop_graph, automaton)
+
+
+    def construct_product(self, prop_graph: PropertyGraph, automaton: ParametricAutomaton):
+        for prop_node, automaton_state in product(prop_graph.nodes, automaton.transitions):
+            self.nodes.add((prop_node, automaton_state))
+            automaton_dsts = automaton.transitions[automaton_state] 
+            graph_dsts = prop_graph.edges[prop_node]
+            for (edge_label_a, constraint), automaton_dst in automaton_dsts:
+                for edge_label_g, graph_dst in graph_dsts:
+                    if edge_label_a == edge_label_g:
+                        self.edges[(prop_node, automaton_state)] = (edge_label_a, constraint, (graph_dst, automaton_dst))            
